ca5ef802d43d43b6f0d4c1f49bf114799a6dc7b1
25bq5A-4311 <kyaswanthbalaganesh2007@gmail.com>
2026-03-07 15:28:20 +0530

    Time: 2 ms (90.20%) | Memory: 48 MB (35.56%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,31 +1,18 @@
 class Solution {
-public static void moveZeroes(int[] nums) {
-int index = 0;  
-for (int i = 0; i < nums.length; i++) {
-if (nums[i] != 0) {
-nums[index] = nums[i];
-index++;
-}
-}
-while (index < nums.length) {
-nums[index] = 0;
-index++;
-}
-}
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.print("Enter size of array: ");
-int n = sc.nextInt();
-int[] nums = new int[n];
-System.out.println("Enter array elements:");
-for (int i = 0; i < n; i++) {
-nums[i] = sc.nextInt();
-}
-moveZeroes(nums);
-System.out.println("Array after moving zeroes:");
-for (int num : nums) {
-System.out.print(num + " ");
-}
-        
+    public void moveZeroes(int[] arr) {
+        int k=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[k++]=arr[i];
+                
+        }
+
+        }
+        while(k<n){
+            arr[k]=0;
+            k++;
+        }
+
     }
 }
